a80fa48bd3222b04e553101ac91a24a3b2b700f4
ayusch00 <ayusch.k@live.de>
2026-07-31 00:15:03 +0200

    delete stuff

--- download_data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd data/cityscapes
-wget --load-cookies cookies.txt "https://www.cityscapes-dataset.com/file-handling/?packageID=1" -O leftImg8bit_trainvaltest.zip
-wget --load-cookies cookies.txt "https://www.cityscapes-dataset.com/file-handling/?packageID=3" -O gtFine_trainvaltest.zip
-unzip gtFine_trainvaltest.zip -d cityscapes/
-unzip leftImg8bit_trainvaltest.zip -d cityscapes/
-rm *.zip
